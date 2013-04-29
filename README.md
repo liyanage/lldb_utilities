8e3efa511861b44960443df96fb9d91547314acd
Marc Liyanage <liyanage@liyanage1.apple.com>
2013-04-29 10:49:38 -0700

    Rework implementation to be class based. Add new dump_nsdata and tempdir commands. Add documentation.

--- README.md
+++ README.md
@@ -14,9 +14,53 @@ Create a `.lldbinit` file in your home directory that loads the custom module:
 
 Launch Xcode, run your target, drop into LLDB and try out the commands as shown below.
 
-# Commands
+# Usage
 
-Here’s a list of the commands that are currently available.
+## Help
+
+Below is a list of the commands that are currently available. You can issue
+
+    command script list
+
+to see a list. For each command, you can get help with LLDB’s `help` command, for example:
+
+    help poc
+
+## Options
+
+Some commands have option switches. You can shorten these as long as the shortened
+version is unambiguous, for example you can shorten
+
+    dump_nsdata -reveal -clipboard foo
+
+to
+
+    dump_nsdata -r -c foo
+
+## Whitespace and Quoting
+
+Some commands expect an expression that yields some object value, in this example `[NSData data]`:
+
+    dump_nsdata -c [NSData data]
+
+Parsing of command options follows shell rules (using Python’s [shlex](http://docs.python.org/2/library/shlex.html) module),
+which means that expressions with spaces like the one above get split up and you would
+normally have to prevent that with quotes:
+
+    dump_nsdata -c "[NSData data]"
+
+As a convenience, you can omit these quotes and the system will reassemble the expression
+by joining the split pieces with spaces. In some cases that approach doesn’t produce the
+correct results:
+
+    dump_nsdata [NSData dataWithContentsOfFile:@"/foo   bar"]
+    
+In these cases, you should quote:
+
+    dump_nsdata '[NSData dataWithContentsOfFile:@"/foo   bar"]'
+
+
+# Commands
 
 ## poc
 
@@ -29,3 +73,45 @@ to the clipboard so you can paste it into an e-mail message or bug report.
 
 the string “&lt;NSApplication: 0x100427590>” is now also on the clipboard.
 
+## dump_nsdata
+
+This command invokes `[data writeToFile:@"xxx" atomically:YES]` for you. Without any options,
+it creates a temporary file inside the directory returned by `NSTemporaryDirectory()`:
+
+    (lldb) dump_nsdata [NSData data]
+    /var/folders/j5/hjm915bx2gd9prgxz9c3fn700000gn/T/nsdata-N4c1ja.dat
+
+You can override that with the `-o` ("output") option:
+
+    (lldb) dump_nsdata -o ~/Desktop/foo.bin someData
+    /Users/you/Desktop/foo.bin
+
+The `-r` ("reveal") option reveals the resulting file in the Finder.
+
+The `-c` ("clipboard") option copies the path to the resulting file to the clipboard.
+
+You can combine these options:
+
+    (lldb) dump_nsdata -c -r -o ~/Desktop/foo.bin someData
+    /Users/you/Desktop/foo.bin
+
+## tempdir
+
+This command prints and copies the return value of `NSTemporaryDirectory()`.
+
+# Extending lldb_utilities
+
+You can write your own commands based on the infrastructure provided by lldb_utilities.py.
+
+To add a command, create a new `DebuggerCommand` subclass and implement the `run` method:
+
+    class DebuggerCommandMyCommand(DebuggerCommand):
+        """Do something awesome."""
+
+        def run(self):
+            # use the lldb API here and do something useful
+            self.result.PutCString('produce some text output')
+
+Your command implementation will automatically be mapped to an LLDB command whose name
+is derived from the class name, in this case `my_command`. Take a look at the existing
+classes to see how to use the convenience methods provided by the base class.
--- lldb_utilities.py
+++ lldb_utilities.py
@@ -10,24 +10,207 @@
 
 import lldb
 import subprocess
+import shlex
+import argparse
+import tempfile
+import os
+import re
+import sys
+
 #import AppKit
 
-def copy_string_to_clipboard(string):
-    pbcopy = subprocess.Popen('pbcopy', stdin=subprocess.PIPE)
-    pbcopy.communicate(string)
 
-# def copy_string_to_clipboard(string):
-#     pb = AppKit.NSPasteboard.generalPasteboard()
-#     pb.clearContents()
-#     pb.writeObjects_(AppKit.NSArray.arrayWithObject_(string))
+class DebuggerCommand(object):
 
-def copy_object_description_to_clipboard(debugger, command, result, internal_dict):
-    """Print the description of an Objective-C object and copy it to the clipboard (Like "po")."""
-    frame = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
-    value = frame.EvaluateExpression(command)
-    object_description = value.GetObjectDescription()
-    print object_description
-    copy_string_to_clipboard(object_description)
+    def __init__(self, debugger, command, result, internal_dict):
+        self.debugger = debugger
+        self.command = command
+        self.result = result
+        self.internal_dict = internal_dict
+        self.expression = None
+        self.args = None
+
+    def run(self):
+        pass
+
+    def needs_expression(self):
+        return True
+
+    def parse_command(self):
+        parser = self.argument_parser()
+        split_args = shlex.split(self.command)
+        try:
+            self.args = parser.parse_args(split_args)
+        except SystemExit as e:
+            self.result.PutCString('Failed to parse arguments: {}'.format(e))
+            self.result.SetStatus(lldb.eReturnStatusFailed)
+            return False
+        
+        if self.needs_expression():
+            self.expression = ' '.join(self.args.expression)
+        
+        return True
+
+    def temporary_directory(self):
+        return self.frame().EvaluateExpression('(NSString *)NSTemporaryDirectory()').GetObjectDescription()
+
+    def frame(self):
+        return self.debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
+
+    def temporary_file_path(self, prefix=None, suffix=None):
+        (handle, path) = tempfile.mkstemp(dir=self.temporary_directory(), prefix=prefix, suffix=suffix)
+        os.close(handle)
+        return path
+
+    def value_for_expression(self, expression):
+        return self.frame().EvaluateExpression(expression)
+
+    def copy_expression_result_to_clipboard(self, expression):
+        self.value_for_expression('(NSInteger)[[NSPasteboard generalPasteboard] clearContents]')
+        self.value_for_expression('(BOOL)[[NSPasteboard generalPasteboard] writeObjects:@[{}]]'.format(expression))
+
+    @classmethod
+    def handle_debugger_command(cls, debugger, command, result, internal_dict):
+        cmd = cls(debugger, command, result, internal_dict)
+        if not cmd.parse_command():
+            return
+        cmd.run()
+
+    @classmethod
+    def command_name(cls):
+        return '_'.join([i.lower() for i in re.findall(r'([A-Z][a-z]+)', re.sub(r'^DebuggerCommand', '', cls.__name__))])
+
+    @classmethod
+    def help_string(cls):
+        return cls.argument_parser().format_help()
+
+    @classmethod
+    def argument_parser(cls):
+        parser = argparse.ArgumentParser(prog=cls.command_name(), description=cls.__doc__)
+        cls.configure_argument_parser(parser)
+        return parser
+
+    @classmethod
+    def configure_argument_parser(cls, parser):
+        pass
+
+# Invoking external tools like "pbcopy" in this case sometimes crashes the inferior
+# because it forks an already multithreaded process, so we don't use it for now
+# and instead use copy_expression_result_to_clipboard above.
+#
+#     @classmethod
+#     def copy_string_to_clipboard(cls, string):
+#         pbcopy = subprocess.Popen('pbcopy', stdin=subprocess.PIPE)
+#         pbcopy.communicate(string)
+
+# Loading the AppKit/Foundation bridge modules into the Python interpreter running
+# inside LLDB doesn't work. If this gets fixed, we can switch to this
+# way of writing to the clipboard.
+#
+#     @classmethod
+#     def copy_string_to_clipboard(cls, string):
+#         pb = AppKit.NSPasteboard.generalPasteboard()
+#         pb.clearContents()
+#         pb.writeObjects_(AppKit.NSArray.arrayWithObject_(string))
+
+
+class DebuggerCommandDumpNsdata(DebuggerCommand):
+    """Dumps NSData instances to a file"""
+
+    def run(self):
+        if self.args.output:
+            self.args.output = os.path.expanduser(self.args.output)
+        else:
+            self.args.output = self.temporary_file_path(prefix='nsdata-', suffix='.dat')
+
+        cmd = '(BOOL)[({}) writeToFile:@"{}" atomically:YES]'.format(self.expression, self.args.output)
+        value = self.value_for_expression(cmd)
+        did_write = value.GetValueAsSigned()
+        if not did_write:
+            self.result.PutCString('Failed to write to {} (permission error?)'.format(self.args.output))
+            self.result.SetStatus (lldb.eReturnStatusFailed)
+            return
+
+        self.result.PutCString(self.args.output)
+
+        if self.args.reveal:
+            cmd = '(void)[[NSWorkspace sharedWorkspace] activateFileViewerSelectingURLs:@[(NSURL *)[NSURL fileURLWithPath:@"{}"]]]'.format(self.args.output)
+            self.value_for_expression(cmd)
+
+        if self.args.clipboard:
+#            self.copy_string_to_clipboard(self.args.output)
+            self.copy_expression_result_to_clipboard('@"{}"'.format(self.args.output))
+        
+    @classmethod
+    def configure_argument_parser(cls, parser):
+        parser.add_argument('expression', nargs='+')
+        parser.add_argument('-output', help='output path')
+        parser.add_argument('-clipboard', action='store_true', help='copy output path to clipboard')
+        parser.add_argument('-reveal', action='store_true', help='reveal output file in Finder')
+
+
+class DebuggerCommandCopyObjectDescriptionToClipboard(DebuggerCommand):
+    """Print the description of an Objective-C object and copy it to the clipboard (Like "po")"""
+
+    def run(self):
+        value = self.value_for_expression(self.command)
+        object_description = value.GetObjectDescription()
+        self.result.PutCString(object_description)
+#        self.copy_string_to_clipboard(object_description)
+        self.copy_expression_result_to_clipboard('[({}) description]'.format(self.command))
+
+    @classmethod
+    def configure_argument_parser(cls, parser):
+        parser.add_argument('expression', nargs='+')
+
+    @classmethod
+    def command_name(cls):
+        return 'poc'
+
+
+class DebuggerCommandTempdir(DebuggerCommand):
+    """Print the value of NSTemporaryDirectory() and copy it to the clipboard"""
+
+    def run(self):
+        path = self.temporary_directory()
+        self.result.PutCString(path)
+#        self.copy_string_to_clipboard(path)
+        self.copy_expression_result_to_clipboard('@"{}"'.format(path))
+
+    def needs_expression(self):
+        return False
+
+
+def invocation_proxy(original_function):
+    def call_proxy(*args):
+        original_function(*args)
+    return call_proxy
+
+def register_handlers(debugger, namespace_name):
+    namespace_module = sys.modules[namespace_name]
+    for k in dir(namespace_module):
+        v = getattr(namespace_module, k)
+        if not (k.startswith('DebuggerCommand') and callable(getattr(v, 'handle_debugger_command', None))):
+            continue
+        command_name = v.command_name()
+        if not command_name:
+            continue
+
+        # Passing the handle_debugger_command bound class method directly
+        # to lldb's "command script add" doesn't work, so we create
+        # a new dummy forwarding function and insert that into a 
+        # known namespace. Passing that to LLDB works. Additionally,
+        # having a separate function for each command lets us assing
+        # the appropriate documentation to its docstring.
+        function_proxy = invocation_proxy(v.handle_debugger_command)
+        function_proxy.__doc__ = v.help_string()
+        function_proxy.__name__ = command_name
+        function_name = 'command_handler_{}'.format(command_name)
+        setattr(namespace_module, function_name, function_proxy)
+
+        cmd = 'command script add -f lldb_utilities.{} {}'.format(function_name, command_name)
+        if debugger:
+            debugger.HandleCommand(cmd)
 
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f lldb_utilities.copy_object_description_to_clipboard poc')
+    register_handlers(debugger, __name__)
